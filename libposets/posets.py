1411e2498f677498ee78eb67d64009b2f917008c
Bree Cummins <breecummins@gmail.com>
2018-10-22 10:17:51 -0600

    resolved fragility in poset code; handled difficult case with a warning and a choice at random

--- libposets/posets.py
+++ libposets/posets.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append('../libposets')
 import libposets.triplet_merge_trees as tmt
 import libposets.sublevel_sets as ss
+from warnings import warn
 
 def get_mins_maxes(name,curve,eps):
     '''
@@ -19,11 +20,11 @@ def get_mins_maxes(name,curve,eps):
     merge_tree_maxs = tmt.births_only(r)
     time_ints_mins = ss.minimal_time_ints(merge_tree_mins,n,eps)
     time_ints_maxs = ss.minimal_time_ints(merge_tree_maxs,r,eps)
-    labeled_mins = sorted([(v,(name," min"),T) for T,v in time_ints_mins.items()])
-    labeled_maxs = sorted([(v,(name," max"),T) for T,v in time_ints_maxs.items()])
+    labeled_mins = sorted([(v,(name," min")) for _,v in time_ints_mins.items()])
+    labeled_maxs = sorted([(v,(name," max")) for _,v in time_ints_maxs.items()])
     nodes = sorted(labeled_mins+labeled_maxs)
     # when a max and min are very close together on a flattish extremum, then the extremum
-    # can have both a max and a min label. The following code picks one.
+    # can have both a max and a min label. The following code finds such pairs.
     prev=0
     new_nodes = []
     for m,p in zip(nodes[:-1],nodes[1:]):
@@ -33,47 +34,32 @@ def get_mins_maxes(name,curve,eps):
             new_nodes.append((m,p))
             prev = p
         else:
-            new_nodes.append(m)
+            new_nodes.append((m,))
     if prev != nodes[-1]:
-        new_nodes+=[nodes[-1]]
-    # must handle case where all nodes are paired
-    # NOTE: len(g)==2 is fragile!! If the length of a labeled extremum changes from 3 to 2, this breaks!!
+        new_nodes+=[(nodes[-1],)]
+    # now choose one of min/max on same interval using information from singletons
+    # must first handle case where all nodes ended up paired
     if all([len(g)==2 for g in new_nodes]):
-        #FIXME: The algorithm in this if clause does not correctly handle single points
-        raise RuntimeError("This case is not implemented.")
-        n0 = new_nodes[0][0]
-        label = n0[1][1].strip()
-        for t in n:
-            tot = 0
-            num = 0
-            if t >= n0[0][0] and t <= n0[0][1]:
-                tot += n[t]
-                num += 1
-        if (n0[2] > tot/num and label == "max") or (n0[2] < tot/num and label == "min"):
-            new_nodes[0] = n0
-        else:
-            new_nodes[0] = new_nodes[0][1]
-    # now get rid of duplicate labels
+        warn("There are two possibilities for the linear order of {}. \nOne is chosen at random.\n To resolve, increase epsilon, which is currently set to {}".format(name,eps))
+        new_nodes[0] = (new_nodes[0][0],)
     while any([len(m)==2 for m in new_nodes]):
         for k,g in enumerate(new_nodes):
             if len(g)==2:
                 if k > 0 and not(len(new_nodes[k-1])==2):
-                    if "max" in new_nodes[k-1][1][1]:
-                        new_nodes[k] = g[0] if "min" in g[0][1][1] else g[1]
+                    if "max" in new_nodes[k-1][0][1][1]:
+                        new_nodes[k] = (g[0],) if "min" in g[0][1][1] else (g[1],)
                         continue
-                    if "min" in new_nodes[k-1][1][1]:
-                        new_nodes[k] = g[0] if "max" in g[0][1][1] else g[1]
+                    if "min" in new_nodes[k-1][0][1][1]:
+                        new_nodes[k] = (g[0],) if "max" in g[0][1][1] else (g[1],)
                         continue
                 if k < len(new_nodes)-1 and not(len(new_nodes[k-1])==2):
-                    if "max" in new_nodes[k+1][1][1]:
-                        new_nodes[k] = g[0] if "min" in g[0][1][1] else g[1]
+                    if "max" in new_nodes[k+1][0][1][1]:
+                        new_nodes[k] = (g[0],) if "min" in g[0][1][1] else (g[1],)
                         continue
-                    if "min" in new_nodes[k+1][1][1]:
-                        new_nodes[k] = g[0] if "max" in g[0][1][1] else g[1]
+                    if "min" in new_nodes[k+1][0][1][1]:
+                        new_nodes[k] = (g[0],) if "max" in g[0][1][1] else (g[1],)
                         continue
-    nodes = [N[:-1] for N in new_nodes]
-    # print(nodes)
-    # print("\n")
+    nodes = [N[0] for N in new_nodes]
     # check that extrema do oscillate
     extrema = [n[1][-3:] for n in nodes]
     if any(x==y for (x,y) in zip(extrema[:-1],extrema[1:])):
@@ -82,6 +68,7 @@ def get_mins_maxes(name,curve,eps):
         raise ValueError("Two minima or two maxima in a row.")
     elif len(extrema)==1:
         # get rid of constant functions
+        warn("Time series {} is constant and is not included in the partial order.".format(name))
         nodes = []
     # make within time series edges; [a,b] < [c,d] only if a < c
     edges = [(i,j) for i, n in enumerate(nodes) for j, m in enumerate(nodes) if n[0][0] < m[0][0]]
