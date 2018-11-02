689a1c0fc509d7a7b9f11eacc6db6200e47f5d87
Bree Cummins <breecummins@gmail.com>
2018-11-02 16:23:22 -0600

    added Riley's code to repository

---
+++ libposets/poset_distance.py
@@ -0,0 +1,109 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2018 Riley Nerem
+#
+# Edited by Bree Cummins
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+
+import networkx as nx
+import numpy as np
+
+
+def dag_distance(G,H):
+    '''
+    G and H are nx.DiGraph objects with node labels, representing ACYCLIC graphs. 
+    Returns the distance between G and H.
+    '''
+    G = nx.transitive_closure(G)
+    H = nx.transitive_closure(H)
+    return len(G.nodes) + len(H.nodes) + len(G.edges) + len(H.edges) - 2*getmaxcommonsubgraphsize(G,H)
+
+
+def makematchinggraph(G,H):
+    # G and H are digraphs, matching_graph is undirected
+    matching_graph = nx.Graph()
+    for i in G.nodes():
+        for j in H.nodes():
+            if G.nodes[i]['label'] == H.nodes[j]['label']:
+                matching_graph.add_node((i,j))
+    for p in matching_graph.nodes():
+        for q in matching_graph.nodes():
+            if G.has_edge(p[0],q[0]) and H.has_edge(p[1],q[1]):
+                matching_graph.add_edge(p,q)
+    return matching_graph
+
+
+def node_in(N,node_dict):
+    for n in node_dict:
+        if N == n[1]:
+            return(True)
+    return(False)
+
+
+def num_edges(node_dict):
+    return(sum([sum([(ne in node_dict) for ne in node_dict[node]]) for node in node_dict])/2)
+
+
+def find_num_nodes(G,H):
+    label_dict_g = {}
+    label_dict_h = {}
+    for ng in G.nodes:
+        if G.nodes[ng]['label'] not in label_dict_g:
+            label_dict_g[G.nodes[ng]['label']] = 1
+        else:
+            label_dict_g[G.nodes[ng]['label']] += 1
+    for nh in H.nodes:
+            if H.nodes[nh]['label'] not in label_dict_h:
+                label_dict_h[H.nodes[nh]['label']] = 1
+            else:
+                label_dict_h[H.nodes[nh]['label']] += 1
+    tot_n = 0
+    for label in label_dict_g:
+        if label in label_dict_h:
+            tot_n+= min(label_dict_g[label],label_dict_h[label])
+    return(tot_n)
+
+
+def getmaxcommonsubgraphsize(G,H):
+    numnodes = find_num_nodes(G,H)
+    G = nx.transitive_closure(G)
+    H = nx.transitive_closure(H)
+    matching_graph = makematchinggraph(G,H)
+    labels = [v for v in matching_graph.nodes()]
+    mg_nodes = dict([(n, [e for e in matching_graph.neighbors(n)]) for n in matching_graph])
+    Gn,Hn = zip(*labels)
+    def pick_nodes(G_list, node_dict):
+        if len(G_list) == 0:
+                return(numnodes+num_edges(node_dict))
+        sizes = 0
+        for node in mg_nodes:
+            if node[0] == G_list[0] and not node_in(node[1], node_dict):
+                #check if node in matching graph corresponds to our node in G and check if node choice is valid
+                sizes=max(sizes,pick_nodes(G_list[1:], {**node_dict,node:mg_nodes[node]}))
+                #pick this node and then make recursive call to pick other nodes
+        if numnodes < len(node_dict) + len(G_list):
+            sizes = max(sizes, pick_nodes(G_list[1:], node_dict))
+        #Check the case in which no node in the mathcing graph corrisponds to G_list[0]
+        return(sizes)
+    #Gn is the list of nodes in G, for each node in G we must pick a node in the matching graph.
+    return(pick_nodes(list(set(Gn)),{}))
+
---
+++ tests/test_poset_distance.py
@@ -0,0 +1,90 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2018 Riley Nerem
+#
+# Edited by Bree Cummins
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import networkx as nx
+import numpy as np
+import random
+import libposets.poset_distance as ldag
+
+def random_graph(N,Nlabels,edge_p):
+    G = nx.DiGraph()
+    rand = np.random.rand(N,N)
+    Gmatrix = (rand <edge_p)
+    Gmatrix[np.tril_indices(N)] = 0
+    G = nx.convert_matrix.from_numpy_matrix(Gmatrix,create_using=G)
+    for n in G.nodes:
+        G.nodes[n]['label'] = str(random.choice(range(1,N+1,1)))
+    return(G)
+
+def benchmark(N,seed=0):
+    np.random.seed(seed)
+    G = random_graph(40,Nlabels = 20,edge_p = .5)
+    H = random_graph(40,Nlabels = 20,edge_p = .5)
+    for i in range(N):
+        print(ldag.dag_distance(G,H))
+
+def example1():
+    G = nx.DiGraph()
+    G.add_nodes_from(range(1,11,1))
+    G.add_edges_from([(1,2),(2,3),(4,5),(5,6),(6,7),(8,9),(9,10),(1,5),(5,3),(8,5),(10,3)])
+    nx.set_node_attributes(G,{1:'xmax',2:'xmin',3:'xmax',4:'ymax',5:'ymin',6:'ymax',7:'ymin',8:'zmin',9:'zmax',10:'zmin'},'label')
+    H = nx.DiGraph()
+    H.add_nodes_from(range(1,7,1))
+    H.add_edges_from([(1,2),(2,3),(4,5),(5,6),(4,2),(2,6)])
+    nx.set_node_attributes(H,{1:'xmax',2:'xmin',3:'xmax',4:'ymin',5:'ymax',6:'ymin'},'label')
+    return(G,H)
+
+
+def example2():
+    G = nx.DiGraph()
+    G.add_nodes_from(range(1,7,1))
+    G.add_edges_from([(1,2),(1,4), (3,4), (5,4), (5,6)])
+    nx.set_node_attributes(G,{1:'xmin',2:'xmax',3:'ymin',4:'ymax',5:'zmax',6:'zmin'},'label')
+    H = nx.DiGraph()
+    H.add_nodes_from(range(1,7,1))
+    H.add_edges_from([(1,2),(1,3),(3,4),(5,4),(5,6)])
+    nx.set_node_attributes(H,{1:'xmin',2:'xmax',3:'ymax',4:'ymin',5:'zmin',6:'zmax'},'label')
+    return(G,H)
+
+def example3():
+    G = nx.DiGraph()
+    G.add_nodes_from(range(1,10,1))
+    G.add_edges_from([(1,2),(2,3), (3,4), (4,5), (6,7),(7,8),(7,1),(8,5)])
+    nx.set_node_attributes(G,{1:'xmin',2:'xmax',3:'xmin',4:'xmax',5:'xmin',6:'ymin',7:'ymax',8:'ymin',9:'zmin'},'label')
+    H = nx.DiGraph()
+    H.add_nodes_from(range(1,8,1))
+    H.add_edges_from([(1,2),(2,3),(4,5),(5,6),(4,3),(2,5)])
+    nx.set_node_attributes(H,{1:'xmax',2:'xmin',3:'xmax',4:'ymax',5:'ymin',6:'ymax',7:'zmax'},'label')
+    return(G,H)
+
+def test_dag():
+    G,H = example1()
+    assert ldag.getmaxcommonsubgraphsize(G,H) == 14
+    assert ldag.dag_distance(G,H) == 21
+    G,H = example2()
+    assert ldag.getmaxcommonsubgraphsize(G,H) == 8
+    assert ldag.dag_distance(G,H) == 7
+    G,H = example3()
+    assert ldag.getmaxcommonsubgraphsize(G,H) == 10
+    assert ldag.dag_distance(G,H) == 31
