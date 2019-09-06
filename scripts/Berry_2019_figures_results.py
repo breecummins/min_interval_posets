52021f58b63ed5116f5b27315acc87b297944d0e
Bree Cummins <breecummins@gmail.com>
2019-09-06 12:02:10 -0600

    attempting to finish introduction examples

--- scripts/Berry_2019_figures_results.py
+++ scripts/Berry_2019_figures_results.py
@@ -1,5 +1,7 @@
 from min_interval_posets.curve import Curve
 from min_interval_posets import posets
+from min_interval_posets import triplet_merge_trees as tmt
+from min_interval_posets import sublevel_sets as ss
 import os
 import pandas
 from copy import deepcopy
@@ -33,14 +35,15 @@ def col(filename):
     return dict(zip(names,curves))
 
 
-def getposets(filename,filestyle,epsilons,names="all",start_time=None,end_time=None):
+def getcurves(filename,filestyle,names,start_time,end_time):
     '''
 
     :param filename: Name of the time series data file. Include absolute or relative path to file.
     :param filestyle: "row" if the time points lie in a single row, or "col" if they lie in a column
-    :param epsilons: list of floats between 0 and 1
-    :param names: list of gene names on which to create poset. If "all" (default), then all genes in the file are used
-    :return: list of partial orders, one for each epsilon
+    :param names: list of gene names on which to create curve. If "all" (default), then all genes in the file are used
+    :param start_time: front trim for time series in time units
+    :param end_time: back trim for time series in time units
+    :return: list of curve objects
     '''
     if filestyle == "row":
         curves = row(os.path.expanduser(filename))
@@ -54,4 +57,75 @@ def getposets(filename,filestyle,epsilons,names="all",start_time=None,end_time=N
             subset_curves.pop(name)
     if start_time is not None and end_time is not None:
         subset_curves = {name : Curve(curve.trim(start_time,end_time)) for name,curve in subset_curves.items()}
+    return subset_curves
+
+
+def getposets(filename,filestyle,epsilons,names="all",start_time=None,end_time=None):
+    '''
+
+    :param filename: Name of the time series data file. Include absolute or relative path to file.
+    :param filestyle: "row" if the time points lie in a single row, or "col" if they lie in a column
+    :param epsilons: list of floats between 0 and 1
+    :param names: list of gene names on which to create poset. If "all" (default), then all genes in the file are used
+    :param start_time: front trim for time series in time units
+    :param end_time: back trim for time series in time units
+    :return: list of partial orders, one for each epsilon
+    '''
+    subset_curves = getcurves(filename,filestyle,names,start_time,end_time)
     return posets.eps_posets(subset_curves, epsilons)
+
+
+def getintervals(filename,filestyle,epsilons,names,start_time,end_time):
+    '''
+
+    :param filename: Name of the time series data file. Include absolute or relative path to file.
+    :param filestyle: "row" if the time points lie in a single row, or "col" if they lie in a column
+    :param epsilons: list of floats between 0 and 1
+    :param names: list of gene names on which to create intervals. If "all" (default), then all genes in the file are used
+    :param start_time: front trim for time series in time units
+    :param end_time: back trim for time series in time units
+    :return: dict of tuples of (eps, intervals), one item for each name in names
+    '''
+
+    subset_curves = getcurves(filename,filestyle,names,start_time,end_time)
+    intervals = dict()
+    for name,curve in subset_curves.items():
+        intervals[name] = []
+        for eps in epsilons:
+            n = curve.normalize()
+            r = curve.normalize_reflect()
+            merge_tree_mins = tmt.births_only(n)
+            merge_tree_maxs = tmt.births_only(r)
+            time_ints_mins = ss.get_sublevel_sets(merge_tree_mins, n, eps)
+            time_ints_maxs = ss.get_sublevel_sets(merge_tree_maxs, r, eps)
+            labeled_mins = sorted([(v, (name, "min")) for _, v in time_ints_mins.items()])
+            labeled_maxs = sorted([(v, (name, "max")) for _, v in time_ints_maxs.items()])
+            # When eps is close to (b-a)/2 for max b and min a, then the intervals can be identical. Annihilate them.
+            nodes = posets.annihilate(sorted(labeled_mins + labeled_maxs))
+            intervals[name].append((eps,nodes))
+    return intervals
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
