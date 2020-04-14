bfe7192c6c898e1b62129267dff3c5857e301048
Bree Cummins <breecummins@gmail.com>
2020-04-14 11:48:19 -0600

    changed import statements to work across os's

--- lib/__init__.py
+++
@@ -1,1 +0,0 @@
-from min_interval_posets import *
--- lib/min_interval_posets/__init__.py
+++ lib/min_interval_posets/__init__.py
@@ -1,1 +1,7 @@
+import min_interval_posets.curve
+import min_interval_posets.poset_distance
+import min_interval_posets.posets
+import min_interval_posets.sublevel_sets
+import min_interval_posets.triplet_merge_trees
+
 __all__ = ["curve","poset_distance","posets","sublevel_sets","triplet_merge_trees"]
